e6b6b9955cd836cb9394c9e8322e92a4fd81a575
Vitali <61989351+vitali007tut@users.noreply.github.com>
2025-09-06 22:41:09 +0300

    deploy test

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({
-    baseURL: '/fake-api',
-});
-
 export const fetchShareholdersData = async () => {
-    await API.get('/shareholders');
     const response = await axios.get('./data.json');
     return response.data;
 };
